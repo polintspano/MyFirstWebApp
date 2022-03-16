25763604dc47032a7c552bf80d8de41e63fe06e8
Polina <phoenix21@abv.bg>
2022-03-16 08:42:25 +0200

    Add supplier entity.

---
+++ MyFirstWebApp/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Models
+{
+    public class Supplier : CommonEntity
+    {
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string Phone { get; set; }
+    }
+}
